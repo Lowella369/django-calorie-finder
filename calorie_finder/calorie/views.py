430725507671ae93c4df63b274abbfc0fccc7bdc
Lowella369 <lh850210@gmail.com>
2020-06-11 15:25:29 -0500

    Adding GetCalorie function in the app view

--- calorie_finder/calorie/views.py
+++ calorie_finder/calorie/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class GetCalorie(TemplateView):
+    template_name = 'calorie/index.html'
+    def get_context_data(self, *args, **kwargs):
+        pass
