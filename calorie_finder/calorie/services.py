1a254734205ad7bd581f858c82163bbfe356503a
Lowella369 <lh850210@gmail.com>
2020-06-11 15:42:31 -0500

    Adding services.py file, used to make API call

---
+++ calorie_finder/calorie/services.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+def get_food():
+	url=''
+  r = requests.get(url, headers={'Authorization':'Bearer %s' % 'access_token'})
+  food = r.json()
+food_list = []
+for i in range(len(food[])):
+	food_list.append(food[][i])
+return food_list
