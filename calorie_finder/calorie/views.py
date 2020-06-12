f29ff29c2dd5cd9f9df747d01ba3ce4cf40c4c4b
Lowella369 <lh850210@gmail.com>
2020-06-12 15:25:50 -0500

    Modified the codes

--- calorie_finder/calorie/views.py
+++ calorie_finder/calorie/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .forms import FoodCalorieForm
 
-def food(request):
+def index(request):
   search_result = {}
-  if 'word' in request.GET:
+  if 'food' in request.GET:
     form = FoodCalorieForm(request.GET)
-    if form.is_Valid()
+    if form.is_valid():
       search_result = form.search()
   else:
     form = FoodCalorieForm()
-  return render(request, 'templates/calorie/index.html', {'form': form, 'search_result': search_result})
+  return render(request, 'calorie/index.html', {'form': form, 'search_result': search_result})
