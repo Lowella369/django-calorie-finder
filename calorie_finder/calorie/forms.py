61219050ce5ca49b612f76a94b79598c8e16339b
Lowella369 <lh850210@gmail.com>
2020-06-12 15:24:45 -0500

    Changing some codes

--- calorie_finder/calorie/forms.py
+++ calorie_finder/calorie/forms.py
@@ -1,24 +1,24 @@
 from django import forms
-from django.config import settings
+from django.conf import settings
 import requests
+from urllib.parse import urlencode
 
 class FoodCalorieForm(forms.Form):
-  word = forms.CharField(max_length=100)
+  food = forms.CharField(max_length=100)
 
   def search(self):
     result={}
-    word = self.cleaned_data['word']
-    endpoint = ''
-    url = endpoint.format(source_lang='en', word_id=word)
+    food = self.cleaned_data['food']
+    url = urlencode('https://api.edamam.com/api/food-database/parser?ingr=' % food)
     headers={'app_id': settings.APP_ID, 'app_key': settings.APP_KEY}
     response = requests.get(url, headers=headers)
     
-    if response.status_code == 200 #SUCCESS
+    if response.status_code == 200:
       result = response.json()
       result['success'] = True
     else:
       result['success'] = False
-      if response.status_code == 404 #NOT FOUND
+      if response.status_code == 404:
         result['message'] = 'No entry found for "%s"' % word
       else:
         result['message'] = 'The EDAMAM API is not available at the moment. Please try again later.'
