2d548d87d1fc82c625d95c3e319428d4869edec9
Lowella369 <lh850210@gmail.com>
2020-06-12 10:31:49 -0500

    Moving the food_list as part of the get_food()

--- calorie_finder/calorie/services.py
+++ calorie_finder/calorie/services.py
@@ -5,7 +5,7 @@ def get_food():
 	url=''
   r = requests.get(url, headers={'Authorization':'Bearer %s' % 'access_token'})
   food = r.json()
-food_list = []
+  food_list = []
 for i in range(len(food[])):
 	food_list.append(food[][i])
 return food_list
