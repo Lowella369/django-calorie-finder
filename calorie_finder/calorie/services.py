e260e5bf5234ae6bc68b5bee4bdc6ff69b02f7db
Lowella369 <lh850210@gmail.com>
2020-06-12 10:36:36 -0500

    Arranged the spacing

--- calorie_finder/calorie/services.py
+++ calorie_finder/calorie/services.py
@@ -2,10 +2,10 @@ import os
 import requests
 
 def get_food():
-	url=''
+  url=''
   r = requests.get(url, headers={'Authorization':'Bearer %s' % 'access_token'})
   food = r.json()
   food_list = []
   for i in range(len(food[])):
-	  food_list.append(food[][i])
-	return food_list
+    food_list.append(food[][i])
+  return food_list
