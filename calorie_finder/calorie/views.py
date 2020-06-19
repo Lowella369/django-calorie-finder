45d01a1a2d87e58cbf7534dd2661ff91ad972af8
Lowella369 <lh850210@gmail.com>
2020-06-19 15:57:12 -0500

    Commiting all changes to the repository

--- calorie_finder/calorie/views.py
+++ calorie_finder/calorie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .forms import FoodCalorieForm
+from django.contrib import messages
+from django.http import JsonResponse
+from calorie.models import Food_PercentValue
 
 def index(request):
   search_result = {}
-  if 'food' in request.GET:
-    form = FoodCalorieForm(request.GET)
+  if 'food' in request.POST:
+    form = FoodCalorieForm(request.POST)
     if form.is_valid():
       search_result = form.search()
   else:
     form = FoodCalorieForm()
   return render(request, 'calorie/index.html', {'form': form, 'search_result': search_result})
+
+  
+  
